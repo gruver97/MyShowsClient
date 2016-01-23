0ee51080d3fb601de98bb96340afafe6fd74fbba
gruver <gruver97@outlook.com>
2016-01-23 21:33:19 +0300

    Ресурсы и пустой сервис.

---
+++ MysShowsClient/Services/MyShow/IMyShowService.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using MysShowsClient.Model;
+
+namespace MysShowsClient.Services.MyShow
+{
+    public interface IMyShowService
+    {
+        Task<List<Description>> SearchShowsAsync(string searchQuery);
+        Task<ExtendedDescription> GetShowDescriptionAsync(int showId);
+    }
+}
---
+++ MysShowsClient/Services/MyShow/MyShowService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using MysShowsClient.Model;
+
+namespace MysShowsClient.Services.MyShow
+{
+    public class MyShowService : IMyShowService
+    {
+        public async Task<List<Description>> SearchShowsAsync(string searchQuery)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<ExtendedDescription> GetShowDescriptionAsync(int showId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
