59a5df250fdcbe1f760d6950e7ae7451f5a4798b
gruver <gruver97@outlook.com>
2016-01-23 18:57:22 +0300

    Стартовый набор библиотек.

---
+++ MysShowsClient/ViewModel/ViewModelLocator.cs
@@ -0,0 +1,27 @@
+/*
+  In App.xaml:
+  <Application.Resources>
+      <vm:ViewModelLocator xmlns:vm="clr-namespace:MysShowsClient"
+                           x:Key="Locator" />
+  </Application.Resources>
+  
+  In the View:
+  DataContext="{Binding Source={StaticResource Locator}, Path=ViewModelName}"
+
+  You can also use Blend to do all this with the tool's support.
+  See http://www.galasoft.ch/mvvm
+*/
+
+using Microsoft.Practices.Unity;
+
+namespace MysShowsClient.ViewModel
+{
+    /// <summary>
+    ///     This class contains static references to all the view models in the
+    ///     application and provides an entry point for the bindings.
+    /// </summary>
+    public class ViewModelLocator
+    {
+        private IUnityContainer _unityContainer = new UnityContainer();
+    }
+}
