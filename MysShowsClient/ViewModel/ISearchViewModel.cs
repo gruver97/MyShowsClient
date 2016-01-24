7215850a47d24040da8c1406d82068944823b85f
gruver <gruver97@outlook.com>
2016-01-24 16:20:51 +0300

    ПРоверка возможнсти выполнения команды.

--- MysShowsClient/ViewModel/ISearchViewModel.cs
+++ MysShowsClient/ViewModel/ISearchViewModel.cs
@@ -1,12 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.ObjectModel;
+using GalaSoft.MvvmLight.Command;
+using MysShowsClient.Model;
 
 namespace MysShowsClient.ViewModel
 {
     public interface ISearchViewModel
     {
+        string SearchQuery { get; set; }
+        ObservableCollection<ShortDescription> ShortDescriptions { get; }
+        RelayCommand SearchCommand { get; } 
     }
 }
--- MysShowsClient/ViewModel/SearchViewModel.cs
+++ MysShowsClient/ViewModel/SearchViewModel.cs
@@ -1,12 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.ObjectModel;
+using Windows.UI.Xaml.Media;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using Microsoft.Practices.Unity;
+using MysShowsClient.Model;
+using MysShowsClient.Services.MyShow;
 
 namespace MysShowsClient.ViewModel
 {
-    public class SearchViewModel:ISearchViewModel
+    public class SearchViewModel : ViewModelBase, ISearchViewModel
     {
+        private readonly IMyShowService _myShowService;
+        private string _searchQuery;
+
+        public string SearchQuery
+        {
+            get { return _searchQuery; }
+            set
+            {
+                if (_searchQuery == value) return;
+                _searchQuery = value;
+                RaisePropertyChanged();
+                SearchCommand.RaiseCanExecuteChanged();
+            }
+        }
+
+        public ObservableCollection<ShortDescription> ShortDescriptions { get; }
+        public RelayCommand SearchCommand { get; }
+
+        public SearchViewModel([Dependency] IMyShowService myShowService)
+        {
+            _myShowService = myShowService;
+            ShortDescriptions = new ObservableCollection<ShortDescription>();
+            SearchCommand = new RelayCommand(() => { }, ()=>!string.IsNullOrWhiteSpace(SearchQuery));
+        }
     }
 }
